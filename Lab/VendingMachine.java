e9d0391daf8709dd88fec69c0816bca679bb2e3f
QuantumDave <dawitseife18@gmail.com>
2026-04-09 19:47:27 +0300

    First working java program for me

---
+++ Lab/VendingMachine.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class VendingMachine{
+	public static void main(String args[]){
+		Scanner keyboard = new Scanner(System.in);
+		int amount;
+		while(true){
+			amount = keyboard.nextInt();
+			if (0 > amount || amount <= 99){break;}
+		}
+			
+		
+		int quarters,dimes,nickels,pennies;
+		quarters = amount / 25;
+		amount = amount % 25;
+		dimes = amount / 10;
+		amount = amount % 10;
+		nickels = amount / 5;
+		amount = amount % 5;
+		pennies = amount;
+
+		System.out.println(quarters + " quarters, " + "\n" + dimes + " Dimes, " + nickels + " nickels, " + pennies + " pennies.");
+	}	
+}
